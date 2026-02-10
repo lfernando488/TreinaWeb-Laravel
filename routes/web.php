7da352857cadbd5126fbe94c6f84e4073b96f9d1
Luiz Fernando <Luiz Fernando>
2026-02-10 02:14:00 -0300

    historico de encomendas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HistoricoController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RastreamentoController;
 use Illuminate\Support\Facades\Route;
@@ -7,3 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class);
 
 Route::get('/rastreamento', RastreamentoController::class)->name('frete.rastreamento');
+
+route::get('/historico', HistoricoController::class)->name('frete.historico');
