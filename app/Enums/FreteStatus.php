0fbdd230131a92ca56b6b39be8c85acb5ea29485
Luiz Fernando <Luiz Fernando>
2026-02-11 00:50:27 -0300

    enums de status

---
+++ app/Enums/FreteStatus.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enums;
+
+enum FreteStatus : string
+{
+    case EM_TRANSITO = 'Em Trânsito';
+    case SAIU_PARA_ENTRAGA = 'Saiu para entrega';
+    case ENTREGUE = 'Entregue';
+
+    public function pegarCorEtiqueta()
+    {
+        return match($this)
+        {
+            self::EM_TRANSITO => 'bg-yellow-100 text-yellow-800',
+            self::SAIU_PARA_ENTRAGA => 'bg-blue-100 text-blue-800',
+            self::ENTREGUE => 'bg-green-100 text-green-800',
+        };
+    }
+
+}
