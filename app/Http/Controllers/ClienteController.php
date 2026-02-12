ec34a4c433985f1d2ec7e2e3fa6dfd9d574078d1
Luiz Fernando <Luiz Fernando>
2026-02-12 00:15:42 -0300

    post de cliente

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    public function store(Request $request)
+    {
+        //dd($request->all());
+
+        $cliente =  Cliente::create($request->all());
+        return $cliente;
+    }
+}
